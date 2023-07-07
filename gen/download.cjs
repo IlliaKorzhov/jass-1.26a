a47b4b5b9ed3da65e7bb0c6892a4ce4c79579f4e
nazarpunk <nazarpunk@gmail.com>
2023-07-08 00:01:36 +0300

    remove ts prepare 2

---
+++ gen/download.cjs
@@ -0,0 +1,29 @@
+const https = require("https");
+const fs = require("fs");
+const download = (url, dest) => {
+    const file = fs.createWriteStream(dest);
+    https.get(url, function (response) {
+        response.pipe(file);
+        file.on('finish', () => {
+            file.close(() => {
+                fs.readFile(dest, {encoding: 'utf8'}, (err, data) => {
+                    if (err) return console.log(err);
+
+                    const result = data.replace(/\r\n/g, '\n')
+                        .replace(/[^\S\r\n]{2,}/g, ' ')
+                        .replace(/\n[^\S\r\n]+/g, '\n');
+
+                    fs.writeFile(dest, result, 'utf8', err => {
+                        if (err) return console.log(err);
+                    });
+                });
+            });
+        });
+    }).on('error', function (err) {
+        console.log(err);
+    });
+};
+
+// https://github.com/UnryzeC/UjAPI/tree/main/uJAPIFiles
+download('https://raw.githubusercontent.com/UnryzeC/UjAPI/main/uJAPIFiles/common.j', './../static/common.j');
+download('https://raw.githubusercontent.com/UnryzeC/UjAPI/main/uJAPIFiles/UjAPI.j', './../static/UjAPI.j');
